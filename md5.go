a8c36fe2e3c89b7ad0c60fbdbb4ce6b33893c323
milinkorath <milinkorath@gmail.com>
2016-02-15 18:57:04 +0530

    removed fmt

--- md5.go
+++ md5.go
@@ -14,11 +14,8 @@ func main() {
 	fm := fs.Mode()
 	var fname string
 	if fm&os.ModeNamedPipe == os.ModeNamedPipe {
-		fmt.Println("Piped Input")
 		md5Hash(inputFile)
 	} else if fm&os.ModeCharDevice == os.ModeCharDevice {
-		fmt.Println("Unix character input")
-		fmt.Println(os.Args)
 		if len(os.Args) > 1 {
 			fname = os.Args[1]
 		} else {
@@ -27,11 +24,8 @@ func main() {
 		inputFile, _ = os.Open(fname)
 		md5Hash(inputFile)
 	} else {
-
-		fmt.Println("Standard Input")
 		md5Hash(inputFile)
 	}
-
 }
 
 func md5Hash(file io.Reader) error {
@@ -41,5 +35,6 @@ func md5Hash(file io.Reader) error {
 	}
 	// hash.Sum(nil)
 	fmt.Printf("%x\n", hash.Sum(nil))
+
 	return nil
 }
