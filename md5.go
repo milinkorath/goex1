38a6ed6290de9c78eb0b0f409db86e6f4fcf76f2
milinkorath <milinkorath@gmail.com>
2016-02-15 21:38:32 +0530

    Final-cleanup

--- md5.go
+++ md5.go
@@ -13,9 +13,10 @@ func main() {
 	fs, _ := inputFile.Stat()
 	fm := fs.Mode()
 	var fname string
+	// Piped input
 	if fm&os.ModeNamedPipe == os.ModeNamedPipe {
 		md5Hash(inputFile)
-	} else if fm&os.ModeCharDevice == os.ModeCharDevice {
+	} else if fm&os.ModeCharDevice == os.ModeCharDevice { // unix character input
 		if len(os.Args) > 1 {
 			fname = os.Args[1]
 		} else {
@@ -23,7 +24,7 @@ func main() {
 		}
 		inputFile, _ = os.Open(fname)
 		md5Hash(inputFile)
-	} else {
+	} else { //stdin input
 		md5Hash(inputFile)
 	}
 }
@@ -33,8 +34,6 @@ func md5Hash(file io.Reader) error {
 	if _, err := io.Copy(hash, file); err != nil {
 		return err
 	}
-	// hash.Sum(nil)
 	fmt.Printf("%x\n", hash.Sum(nil))
-
 	return nil
 }
